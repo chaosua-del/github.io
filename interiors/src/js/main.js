de8f6244c9450c55cb5673af169e1e17632f41cf
chaosua-del <54207425+chaosua-del@users.noreply.github.com>
2020-04-13 19:19:45 +0300

    feat: finally  finished portfolio

--- interiors/src/js/main.js
+++ interiors/src/js/main.js
@@ -4,18 +4,42 @@ $('document').ready(function () {
   })
 
   var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 4,
+    slidesPerView: 3,
+    centeredSlides: false,
     navigation: {
       nextEl: '.portfolio__swiper-button-next',
       prevEl: '.portfolio__swiper-button-prev',
     },
   });
 
-  // появление кнопки на слайдере
-  $('.portfolio__swiper-slide--add').mouseover(function () {
-    $('.slide-add-button').css("opacity", "1");
-  }).mouseout(function () {
-    $('.slide-add-button').css("opacity", "0");
-  });
+  // // появление кнопки на слайдере
+  // $('.portfolio__swiper-slide--add').mouseover(function () {
+  //   $('.slide-add-button').css("opacity", "1");
+  // }).mouseout(function () {
+  //   $('.slide-add-button').css("opacity", "0");
+  // });
+
+  $(window).on('resize', function () {
+    if ($(window).width() < 1250) {
+      var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 'auto',
+        navigation: {
+          nextEl: '.portfolio__swiper-button-next',
+          prevEl: '.portfolio__swiper-button-prev',
+        },
+      });
+    }
+  })
+
+  if ($(window).width() < 1250) {
+    var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 'auto',
+      navigation: {
+        nextEl: '.portfolio__swiper-button-next',
+        prevEl: '.portfolio__swiper-button-prev',
+      },
+    });
+  }
+
 
 });
